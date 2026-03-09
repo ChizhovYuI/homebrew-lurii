ee7c77446f6e8caafa2a5e9e60a2eec7b76ef53a
Yurii Chizhov <ychizhov@anna.money>
2026-03-09 12:35:43 +0700

    Bump lurii-finance to 2.9.11

--- Casks/lurii-finance.rb
+++ Casks/lurii-finance.rb
@@ -1,6 +1,6 @@
 cask "lurii-finance" do
-  version "2.9.10"
-  sha256 "5c6c177fa9e143639327a92e8e0acb70f9ea450ea54aa1f58088d7424aac1dfc"
+  version "2.9.11"
+  sha256 "367dcf79a073baa87d8e919f1a192d112e6307ad81e5e7695914c40db15cc08f"
 
   url "https://github.com/ChizhovYuI/lurii-finance/releases/download/v#{version}/LuriiFinance-#{version}.zip"
   name "Lurii Finance"
